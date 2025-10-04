852aa73b365c061014753b96bdb2ae82aa06b2c2
José Thomaz <jose.thoomaz@gmail.com>
2025-10-04 00:53:19 -0300

    build: inicializa projeto spring boot

---
+++ backend/atendimento/src/main/java/br/org/apae/atendimento/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package br.org.apae.atendimento.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
