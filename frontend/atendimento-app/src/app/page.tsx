74a9c8a28925b3cab78180a5b3cb8da2c988c160
Thales <thalesluiz45@hotmail.com>
2025-10-05 20:27:09 -0300

    chore: cria estrutura inicial do projeto

---
+++ frontend/atendimento-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="font-sans flex items-center justify-center h-screen">
+      <main>
+        <div>Hello World!</div>
+      </main>
+    </div>
+  );
+}
