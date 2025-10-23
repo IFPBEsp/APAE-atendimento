27a0e9c4030a9e302e855fefb4bf35ec40f0e0ab
Thoomaz <jose.thoomaz@gmail.com>
2025-10-23 15:29:53 -0300

    feat: criação do service 'ProfissionalSaudeService'

---
+++ backend/atendimento/src/main/java/br/org/apae/atendimento/services/ProfissionalSaudeService.java
@@ -0,0 +1,32 @@
+package br.org.apae.atendimento.services;
+
+import br.org.apae.atendimento.entities.Paciente;
+import br.org.apae.atendimento.entities.ProfissionalSaude;
+import br.org.apae.atendimento.repositories.ProfissionalSaudeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProfissionalSaudeService {
+
+    private final ProfissionalSaudeRepository profissionalSaudeRepository;
+
+    public ProfissionalSaudeService(ProfissionalSaudeRepository profissionalSaudeRepository) {
+        this.profissionalSaudeRepository = profissionalSaudeRepository;
+    }
+
+    public ProfissionalSaude getProfissionalById(Long id) {
+        return profissionalSaudeRepository.findById(id).orElseThrow();
+    }
+
+    public List<Paciente> getPacienteDoProfissional (Long id) {
+        ProfissionalSaude profissionalSaude = getProfissionalById(id);
+        return profissionalSaude.getPacientes();
+    }
+
+    public String getPrimeiroNome (Long id) {
+        ProfissionalSaude profissionalSaude = getProfissionalById(id);
+        return profissionalSaude.getPrimeiroNome();
+    }
+}
