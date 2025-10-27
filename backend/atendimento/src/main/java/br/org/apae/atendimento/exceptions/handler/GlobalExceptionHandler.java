848df9c420f541690f14024150322654d2f8e3eb
jefofs <jeferson.freire@academico.ifpb.edu.br>
2025-10-27 07:01:13 -0300

    feat: pacote de handler criado

--- backend/atendimento/src/main/java/br/org/apae/atendimento/exceptions/handler/GlobalExceptionHandler.java
+++ backend/atendimento/src/main/java/br/org/apae/atendimento/exceptions/handler/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
-package br.org.apae.atendimento.exceptions;
+package br.org.apae.atendimento.exceptions.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import br.org.apae.atendimento.exceptions.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
