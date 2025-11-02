a17344b05538ba8c83f1b816ef3c9fdbd16e99d1
jefofs <jeferson.freire@academico.ifpb.edu.br>
2025-11-02 16:35:20 -0300

    feat: controllers criados para todas os services

---
+++ backend/atendimento/src/main/java/br/org/apae/atendimento/controllers/ConsultaController.java
@@ -0,0 +1,47 @@
+package br.org.apae.atendimento.controllers;
+
+import br.org.apae.atendimento.entities.Consulta;
+import br.org.apae.atendimento.entities.Paciente;
+import br.org.apae.atendimento.entities.ProfissionalSaude;
+import br.org.apae.atendimento.services.ConsultaService;
+import br.org.apae.atendimento.services.PacienteService;
+import br.org.apae.atendimento.services.ProfissionalSaudeService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/consultas")
+public class ConsultaController {
+
+    @Autowired
+    private ConsultaService consultaService;
+
+    @Autowired
+    private PacienteService pacienteService;
+
+    @Autowired
+    private ProfissionalSaudeService profissionalSaudeService;
+
+    @PostMapping
+    public ResponseEntity<Consulta> criarConsulta(
+            @RequestBody Consulta consulta,
+            @RequestParam Long pacienteId,
+            @RequestParam Long profissionalId
+    ) {
+        Paciente paciente = pacienteService.getPacienteById(pacienteId);
+        ProfissionalSaude profissional = profissionalSaudeService.getProfissionalById(profissionalId);
+
+        Consulta novaConsulta = consultaService.addConsulta(consulta, paciente, profissional);
+        return ResponseEntity.ok(novaConsulta);
+    }
+
+    @GetMapping("/paciente/{id}")
+    public ResponseEntity<List<Consulta>> listarConsultasDoPaciente(@PathVariable Long id) {
+        List<Consulta> consultas = consultaService.getConsultasDoPaciente(id);
+        return ResponseEntity.ok(consultas);
+    }
+}
