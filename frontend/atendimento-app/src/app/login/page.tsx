c40434c47911beac43c0589e2d63661ff263a029
Thales <thalesluiz45@hotmail.com>
2025-11-04 19:02:43 -0300

    feat: aplicando componentes do shadcn

--- frontend/atendimento-app/src/app/login/page.tsx
+++ frontend/atendimento-app/src/app/login/page.tsx
@@ -1,61 +1,164 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import Image from 'next/image';
-import { Mail, LogIn, Info } from 'lucide-react';
+import { useState } from "react";
+import Image from "next/image";
+import { Mail, Info } from "lucide-react";
+import { Nunito, Baloo_2 } from "next/font/google";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupInput,
+  InputGroupText,
+  InputGroupTextarea,
+} from "@/components/ui/input-group";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+const nunitoFont = Nunito({
+  weight: "700",
+});
+
+const baloo2Font = Baloo_2({
+  weight: "500",
+});
+
+const novoemail = "thalesluiz@hotmail.com";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+
   const [touched, setTouched] = useState(false);
 
   const isValidEmail = (value: string) =>
     /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
 
-  const hasError = touched && !isValidEmail(email) && email !== '';
+  const hasError = touched && !isValidEmail(email) && email !== "";
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+    if (email !== "algo@hotmail.com") {
+      setError("E-mail inválido.");
+      return;
+    }
     if (!isValidEmail(email)) return;
     alert(`Login com o e-mail: ${email}`);
   };
 
   return (
-    <div
+    <div className="h-screen w-screen bg-[url('/background-login-apae.svg')] bg-no-repeat bg-cover bg-center flex items-center justify-center">
+      <div className="absolute flex items-center justify-center inset-0 bg-[#0D4F97]/80">
+        <Card className="w-[410px] h-[455px] rounded-[30px]">
+          <CardContent>
+            <form onSubmit={handleSubmit}>
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-2 ">
+                  <Label htmlFor="email">Email</Label>
+
+                  <InputGroup className="h-[46px] rounded-full w-full bg-white text-[16px] font-normal text-gray-700">
+                    <InputGroupInput
+                      type="email"
+                      value={email}
+                      placeholder="email@email.com"
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                    />
+                    <InputGroupAddon>
+                      <Mail />
+                    </InputGroupAddon>
+                    <InputGroupAddon align="inline-end">
+                      <Info />
+                    </InputGroupAddon>
+                  </InputGroup>
+                </div>
+              </div>
+
+              <div className="h-6 mt-[6px] mb-[19px]">
+                {error && <p className="text-red-500 text-sm mb-3">{error}</p>}
+              </div>
+
+              <Button
+                type="submit"
+                disabled={!isValidEmail(email)}
+                className={`active:scale-[0.98] w-full h-[46px] rounded-full bg-[#165BAA] disabled:bg-[#B0C6DE] hover:bg-[#13447D] text-[18px] hover:cursor-pointer ${baloo2Font.className}`}
+                style={{ boxShadow: "4px 4px 10px rgba(0, 0, 0, 0.25)" }}
+              >
+                Entrar
+              </Button>
+
+              <div className="flex items-center my-[16px]">
+                <hr className="flex-grow border-gray-300" />
+                <span className="mx-2 text-gray-500 text-sm">ou</span>
+                <hr className="flex-grow border-gray-300" />
+              </div>
+
+              <Button
+                type="button"
+                className={` active:scale-[0.98] w-full h-[46px] rounded-full border border-[#B2D7EC] bg-white  text-gray-700 text-[18px] hover:cursor-pointer hover:bg-[#F8FAFD] ${baloo2Font.className}`}
+                style={{ boxShadow: "4px 4px 10px rgba(0, 0, 0, 0.25)" }}
+              >
+                <Image
+                  src="/google-icon.svg"
+                  alt="Google"
+                  width={25}
+                  height={25}
+                />
+                Entrar com o Google
+              </Button>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+
+    /* <div
       className="
         flex items-center justify-center min-h-screen
         bg-cover bg-center bg-no-repeat
+        bg-[url('/background-login-apae.svg')] 
       "
-      style={{ backgroundImage: "url('/background-login-apae.svg')" }}
     >
-     
       <div className="bg-white p-8 rounded-2xl shadow-lg w-full max-w-sm text-center">
-
         <div className="flex flex-col items-center mb-6">
           <Image src="/logo-apae.svg" alt="Logo APAE" width={60} height={60} />
         </div>
 
         <form onSubmit={handleSubmit} className="space-y-5">
           <div className="text-left">
-            <label className="block font-bold text-[18px] text-gray-700 mb-1">
-              Email do profissional <span className="text-red-500">*</span>
+            <label
+              className={`block text-[18px] text-gray-700 mb-1 ${nunitoFont.className}`}
+            >
+              Email do profissional <span className="text-orange-400">*</span>
             </label>
             <div
               className={`relative flex items-center h-[46px] rounded-full border transition-colors ${
                 hasError
-                  ? 'border-red-500'
+                  ? "border-[#F45D6C]"
                   : email
-                  ? 'border-blue-600'
-                  : 'border-[#D9E1E8]'
+                  ? "border-[#0D4F97]"
+                  : "border-[#D9E1E8]"
               }`}
             >
               <Mail
                 size={20}
                 className={`absolute left-4 ${
                   hasError
-                    ? 'text-red-500'
+                    ? "text-[#F45D6C]"
                     : email
-                    ? 'text-blue-600'
-                    : 'text-gray-400'
+                    ? "text-blue-600"
+                    : "text-gray-400"
                 }`}
               />
               <input
@@ -70,60 +173,43 @@ export default function LoginPage() {
                 size={20}
                 className={`absolute right-4 ${
                   hasError
-                    ? 'text-red-500'
+                    ? "text-[#F45D6C]"
                     : email
-                    ? 'text-blue-600'
-                    : 'text-gray-400'
+                    ? "text-blue-600"
+                    : "text-gray-400"
                 }`}
               />
             </div>
             {hasError && (
-              <p className="text-red-500 text-sm mt-1">Email inválido.</p>
+              <p className="text-[#F45D6C] text-sm mt-2.5">Email inválido.</p>
             )}
           </div>
 
-          <button
+          <Button
             type="submit"
             disabled={!isValidEmail(email)}
-            className="
-              w-full h-[46px]
-              bg-blue-600 text-white text-[18px] font-medium
-              rounded-full
-              flex items-center justify-center gap-3
-              transition
-              hover:bg-blue-700
-              disabled:bg-blue-300 disabled:cursor-not-allowed
-              cursor-pointer
-            "
+            className={`active:scale-[0.98] w-full h-[46px] rounded-full bg-[#165BAA] disabled:bg-[#B0C6DE] hover:bg-[#13447D] text-[18px] hover:cursor-pointer ${baloo2Font.className}`}
+            style={{ boxShadow: "4px 4px 10px rgba(0, 0, 0, 0.25)" }}
           >
-            <LogIn size={25} />
             Entrar
-          </button>
+          </Button>
 
-          <div className="flex items-center my-3">
+          <div className="flex items-center">
             <hr className="flex-grow border-gray-300" />
             <span className="mx-2 text-gray-500 text-sm">ou</span>
             <hr className="flex-grow border-gray-300" />
           </div>
 
-          <button
+          <Button
             type="button"
-            className="
-              w-full h-[46px]
-              border border-gray-300 rounded-full
-              flex items-center justify-center gap-[12px]
-              text-gray-700 font-medium
-              transition-all duration-150
-              hover:bg-gray-100 hover:shadow-md hover:cursor-pointer
-              active:scale-[0.98]
-              shadow-sm
-            "
+            className={` active:scale-[0.98] w-full h-[46px] rounded-full border border-[#B2D7EC] bg-white  text-gray-700 text-[18px] hover:cursor-pointer hover:bg-[#F8FAFD] ${baloo2Font.className}`}
+            style={{ boxShadow: "4px 4px 10px rgba(0, 0, 0, 0.25)" }}
           >
             <Image src="/google-icon.svg" alt="Google" width={25} height={25} />
             Entrar com o Google
-          </button>
+          </Button>
         </form>
       </div>
-    </div>
+    </div>*/
   );
 }
