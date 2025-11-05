dffea7ca94a8aede0174bbe37ce7bb65c2478dd8
Thales <thalesluiz45@hotmail.com>
2025-11-04 23:43:18 -0300

    feat: tela de login responsiva

--- frontend/atendimento-app/src/app/login/page.tsx
+++ frontend/atendimento-app/src/app/login/page.tsx
@@ -1,26 +1,15 @@
 "use client";
 
 import { useState } from "react";
-import Image from "next/image";
+
 import { Mail, Info } from "lucide-react";
 import { Nunito, Baloo_2 } from "next/font/google";
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 
 import {
   InputGroup,
   InputGroupAddon,
-  InputGroupButton,
   InputGroupInput,
-  InputGroupText,
-  InputGroupTextarea,
 } from "@/components/ui/input-group";
 
 import { Label } from "@/components/ui/label";
@@ -33,20 +22,12 @@ const baloo2Font = Baloo_2({
   weight: "500",
 });
 
-const novoemail = "thalesluiz@hotmail.com";
-
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [error, setError] = useState("");
 
-  const [touched, setTouched] = useState(false);
-
-  const [isFocused, setIsFocused] = useState(false);
-
   const isValidEmail = (value: string) =>
     /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
-
-  const hasError = touched && !isValidEmail(email) && email !== "";
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -59,20 +40,19 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="h-screen w-screen bg-[url('/background-login-apae.svg')] bg-no-repeat bg-cover bg-center flex items-center justify-center">
+    <div className="h-screen w-screen bg-[url('/background-login-apae.svg')] relative bg-no-repeat bg-cover bg-center flex items-center justify-center">
       <div className="absolute flex items-center justify-center inset-0 bg-[#0D4F97]/80">
-        <Card className="w-[410px] h-[455px] rounded-[30px]">
-          <div className="w-[100px] h-[100px] bg-white rounded-full flex items-center justify-center mx-auto mb-[0px]">
-            <Image
-              src="/logo-apae.svg" // ou o caminho correto da sua logo
-              alt="APAE Logo"
-              width={60}
-              height={60}
-            />
-          </div>
-          <CardContent className="flex flex-col justify-end h-full pb-[45px]">
+        <div className="absolute z-50 mt-[-28rem] w-[128px] h-[128px] bg-white rounded-full flex items-center justify-center mx-auto mb-[0px]">
+          <img
+            src="/logo-apae.svg"
+            alt="APAE Logo"
+            className="w-[90px] h-[140px] mt-[3rem]"
+          />
+        </div>
+        <Card className="w-[380px] h-[455px] rounded-[30px] sm:w-[410px]">
+          <CardContent className="flex flex-col justify-end h-full pb-6 ">
             <form onSubmit={handleSubmit}>
-              <div className="flex flex-col gap-6">
+              <div className=" flex flex-col gap-6 ">
                 <div className="grid gap-2 ">
                   <Label
                     htmlFor="email"
@@ -82,24 +62,21 @@ export default function LoginPage() {
                     <span className="text-[#F28C38]">*</span>
                   </Label>
 
-                  <InputGroup className="h-[46px] rounded-full w-full bg-white text-[16px] font-normal text-gray-700 focus-within:ring-2 ">
+                  <InputGroup className="h-[46px] rounded-full w-full bg-white font-normal text-gray-700 focus-within:ring-2">
                     <InputGroupInput
+                      className={` ${nunitoFont.className}`}
                       type="email"
                       value={email}
-                      placeholder="email@email.com"
+                      placeholder="seuemail@hotmail.com"
                       onChange={(e) => {
                         setEmail(e.target.value);
                         setError("");
                       }}
                       aria-invalid={error !== ""}
                       required
-                      className="focus:outline-none"
                     />
                     <InputGroupAddon>
                       <Mail />
-                    </InputGroupAddon>
-                    <InputGroupAddon align="inline-end">
-                      <Info />
                     </InputGroupAddon>
                   </InputGroup>
                 </div>
@@ -119,9 +96,9 @@ export default function LoginPage() {
               </Button>
 
               <div className="flex items-center my-[16px]">
-                <hr className="flex-grow border-gray-300" />
+                <hr className="flex-grow border-[#B2D7EC]" />
                 <span className="mx-2 text-gray-500 text-sm">ou</span>
-                <hr className="flex-grow border-gray-300" />
+                <hr className="flex-grow border-[#B2D7EC]" />
               </div>
 
               <Button
@@ -129,11 +106,10 @@ export default function LoginPage() {
                 className={` active:scale-[0.98] w-full h-[46px] rounded-full border border-[#B2D7EC] bg-white  text-gray-700 text-[18px] hover:cursor-pointer hover:bg-[#F8FAFD] ${baloo2Font.className}`}
                 style={{ boxShadow: "4px 4px 10px rgba(0, 0, 0, 0.25)" }}
               >
-                <Image
+                <img
                   src="/google-icon.svg"
                   alt="Google"
-                  width={25}
-                  height={25}
+                  className="w-[25px] h-[25px]"
                 />
                 Entrar com o Google
               </Button>
