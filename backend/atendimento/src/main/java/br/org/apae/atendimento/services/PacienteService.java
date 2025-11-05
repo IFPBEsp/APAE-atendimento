3ee1d0ccd5546a6904ab09b4d2ad10e1023721be
Filipe Kevyn <FilipeKevyn4@gmail.com>
2025-11-05 02:01:37 -0300

    fix: Adicionando anotacoes spring

--- backend/atendimento/src/main/java/br/org/apae/atendimento/services/PacienteService.java
+++ backend/atendimento/src/main/java/br/org/apae/atendimento/services/PacienteService.java
@@ -2,8 +2,9 @@ package br.org.apae.atendimento.services;
 import br.org.apae.atendimento.entities.Paciente;
 import br.org.apae.atendimento.exceptions.PacienteNotFoundException;
 import br.org.apae.atendimento.repositories.PacienteRepository;
+import org.springframework.stereotype.Service;
 
-
+@Service
 public class PacienteService {
     private PacienteRepository pacienteRepository;
 
