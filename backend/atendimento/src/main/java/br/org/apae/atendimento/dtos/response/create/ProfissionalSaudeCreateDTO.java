0d9eac1e370f533d19cc6d4ab04df6be136627ec
o-Arcanjo <148638458+o-Arcanjo@users.noreply.github.com>
2025-11-12 13:58:13 +0000

    feat:adicionar a camada dto ao backend

---
+++ backend/atendimento/src/main/java/br/org/apae/atendimento/dtos/response/create/ProfissionalSaudeCreateDTO.java
@@ -0,0 +1,8 @@
+public record ProfissionalSaudeCreateDTO(
+    Long id,
+    String primeiro_nome,
+    String nome_completo,
+    String email,
+    String contato,
+    String crm
+){}
---
+++ backend/atendimento/src/main/java/br/org/apae/atendimento/dtos/response/find/ProfissionalSaudeFindPrimeiroNome.java
@@ -0,0 +1,3 @@
+public record ProfissionalSaudeFindPrimeiroNome(
+     String primeiro_nome
+){}
