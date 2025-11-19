273df735c9630a3e41a9d9d03215c69e4f157190
jhenniferknc <jhenniferkelly4@gmail.com>
2025-11-19 11:02:25 -0300

    feat: implementação do perfil do profissional

--- frontend/atendimento-app/src/components/shared/header.tsx
+++ frontend/atendimento-app/src/components/shared/header.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { Button } from "../ui/button";
 import { LogOut } from "lucide-react";
 import { Menu } from "lucide-react";
+import { MeusDadosModal } from "../modals/meusDados";
 
 export default function Header() {
   const [open, setOpen] = useState(false);
@@ -14,6 +15,7 @@ export default function Header() {
     <>
       <header className="w-full border-b bg-[#F8FAFD] shadow-sm">
         <div className="flex items-center justify-between px-6 py-3 w-full">
+          
           <Link href="/home" className="flex items-center gap-3">
             <Image
               src="/APAE-logo.svg"
@@ -37,13 +39,19 @@ export default function Header() {
           {/*Menu web*/}
           <div className="hidden md:flex items-center w-full">
             <div className="flex-1 flex justify-center gap-10 text-sm font-medium">
+              
               <Link href="/home" className="text-[#344054] hover:underline">
                 Pacientes
               </Link>
 
-              <Link href="#" className="text-[#344054] hover:underline">
-                Meus dados
-              </Link>
+              <MeusDadosModal
+                trigger={
+                  <button className="text-[#344054] hover:underline font-medium">
+                    Meus dados
+                  </button>
+                }
+              />
+
             </div>
 
             <button className="text-red-500 text-sm font-medium flex items-center gap-2 cursor-pointer">
@@ -56,13 +64,18 @@ export default function Header() {
       {/* Menu dropdown mobile*/}
       {open && (
         <div className="w-full bg-white shadow-md py-4 flex flex-col items-center gap-4 border-b md:hidden">
+          
           <Link href="/home" className="text-[#344054] text-sm hover:underline">
             Pacientes
           </Link>
 
-          <Link href="#" className="text-[#344054] text-sm hover:underline">
-            Meus dados
-          </Link>
+          <MeusDadosModal
+            trigger={
+              <button className="text-[#344054] test-sm hover:underline">
+                Meus dados
+              </button>
+            }
+          />
 
           <button className="text-red-500 text-sm font-medium flex items-center gap-2 hover:underline">
             <LogOut className="w-4 h-4" /> Sair
