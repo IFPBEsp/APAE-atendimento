31dfeace02f7cfcb36cd71e0da09edc6781938a4
José Thomaz <jose.thoomaz@gmail.com>
2025-11-19 09:57:25 -0300

    Fix: retira Menu Hamburguer e links do Header

--- frontend/atendimento-app/src/components/shared/header.tsx
+++ frontend/atendimento-app/src/components/shared/header.tsx
@@ -2,21 +2,9 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
-import { Button } from "../ui/button";
-import { LogOut } from "lucide-react";
-import { Menu } from "lucide-react";
-import { useRouter } from "next/navigation";
 
 export default function Header() {
-  const [open, setOpen] = useState(false);
-  const router = useRouter();
 
-  function logout() {
-    document.cookie =
-      "verified=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT;";
-    router.push("/login");
-  }
   return (
     <>
       <header className="w-full border-b bg-[#F8FAFD] shadow-sm">
@@ -32,53 +20,11 @@ export default function Header() {
             <span className="font-bold text-lg text-[#344054]">APAE</span>
           </Link>
 
-          {/* Menu mobile*/}
-          <Button
-            variant="ghost"
-            className="h-10 w-10 p-2 md:hidden"
-            onClick={() => setOpen(!open)}
-          >
-            <Menu className="w-6 h-6 text-[#344054]" />
-          </Button>
-
-          {/*Menu web*/}
-          <div className="hidden md:flex items-center w-full">
-            <div className="flex-1 flex justify-center gap-10 text-sm font-medium">
-              <Link href="/home" className="text-[#344054] hover:underline">
-                Pacientes
-              </Link>
-
-              <Link href="#" className="text-[#344054] hover:underline">
-                Meus dados
-              </Link>
-            </div>
-
-            <button
-              onClick={logout}
-              className="text-red-500 text-sm font-medium flex items-center gap-2 cursor-pointer"
-            >
-              <LogOut className="w-4 h-4" /> Sair
-            </button>
-          </div>
+          
+          
         </div>
       </header>
 
-      {/* Menu dropdown mobile*/}
-      {open && (
-        <div className="w-full bg-white shadow-md py-4 flex flex-col items-center gap-4 border-b md:hidden">
-          <Link href="/home" className="text-[#344054] text-sm hover:underline">
-            Pacientes
-          </Link>
-
-          <Link href="#" className="text-[#344054] text-sm hover:underline">
-            Meus dados
-          </Link>
-
-          <button className="text-red-500 text-sm font-medium flex items-center gap-2 hover:underline">
-            <LogOut className="w-4 h-4" /> Sair
-          </button>
-        </div>
-      )}
     </>
   );
 }
