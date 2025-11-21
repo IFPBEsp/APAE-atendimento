d938eea90f53011966fc04e3a4914e9997dddb07
o-Arcanjo <oarcanjovalentim@gmail.com>
2025-11-21 16:09:46 -0300

    feat: adicionar camada mapper para conversã das entidades

---
+++ backend/atendimento/src/main/java/br/org/apae/atendimento/mappers/ProfissionalMapper.java
@@ -0,0 +1,24 @@
+package br.org.apae.atendimento.mappers;
+
+import br.org.apae.atendimento.dtos.response.ProfissionalResponseDTO;
+import br.org.apae.atendimento.entities.ProfissionalSaude;
+
+public class ProfissionalMapper extends AbstractMapper<ProfissionalSaude, Void, ProfissionalResponseDTO>{
+    @Override
+    public ProfissionalSaude toEntityPadrao(Void dtoPadraoProfissionalSaude) {
+        return null;
+    }
+
+    @Override
+    public ProfissionalResponseDTO toDTOPadrao(ProfissionalSaude profissional) {
+        return new ProfissionalResponseDTO(
+                profissional.getId(),
+                profissional.getPrimeiroNome(),
+                profissional.getNomeCompleto(),
+                profissional.getEmail(),
+                profissional.getContato(),
+                profissional.getCrm()
+        );
+    }
+
+}
