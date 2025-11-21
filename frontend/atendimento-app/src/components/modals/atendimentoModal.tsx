7dee2e71a026dbfcb632c19a6bf3a7e17ec8c691
Thales <thalesluiz45@hotmail.com>
2025-11-21 14:40:37 -0300

    refactor: correção de conflitos

--- frontend/atendimento-app/src/components/modals/atendimentoModal.tsx
+++ frontend/atendimento-app/src/components/modals/atendimentoModal.tsx
@@ -1,11 +1,13 @@
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
 import { ReactNode } from "react";
 import { Nunito } from "next/font/google";
+import { X } from "lucide-react";
 
 interface AtendimentoModalProps {
   open: boolean;
@@ -25,8 +27,14 @@ export function AtendimentoModal({
       <DialogContent
         className={`sm:max-w-[632px] rounded-[30px] max-h-[560px] overflow-scroll ${nunitoFont.className}`}
       >
-        <DialogHeader>
-          <DialogTitle className="text-center text-[#344054]">
+        <div className="absolute right-5 top-5 z-10">
+          <DialogClose className="rounded-full p-1 hover:bg-gray-100 transition-colors outline-none">
+            <X className="h-6 w-6 text[]" />
+          </DialogClose>
+        </div>
+
+        <DialogHeader className="mt-4">
+          <DialogTitle className=" text-xl text-center text-[#344054] ">
             Adicionar novo atendimento
           </DialogTitle>
         </DialogHeader>
--- frontend/atendimento-app/src/components/modals/meusDados.tsx
+++ frontend/atendimento-app/src/components/modals/meusDados.tsx
@@ -1,6 +1,13 @@
-'use client';
+"use client";
 
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger, DialogClose } from "../ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from "../ui/dialog";
 import { Button } from "../ui/button";
 import { LogOut, X } from "lucide-react";
 
@@ -8,17 +15,12 @@ interface MeusDadosModalProps {
   trigger: React.ReactNode;
 }
 
-export function MeusDadosModal({
-  trigger
-}: MeusDadosModalProps) {
+export function MeusDadosModal({ trigger }: MeusDadosModalProps) {
   return (
     <Dialog>
-      <DialogTrigger asChild>
-        {trigger}
-      </DialogTrigger>
+      <DialogTrigger asChild>{trigger}</DialogTrigger>
 
       <DialogContent className="max-w-[90%] sm:max-w-[500px] p-0 border-none rounded-[30px] bg-white font-sans">
-
         <div className="absolute right-5 top-5 z-10">
           <DialogClose className="rounded-full p-1 hover:bg-gray-100 transition-colors outline-none">
             <X className="h-6 w-6 text[]" />
@@ -26,7 +28,6 @@ export function MeusDadosModal({
         </div>
 
         <div className="px-8 py-8 flex flex-col items-center">
-
           <DialogHeader className="mb-4 w-full">
             <DialogTitle className="text-2xl font-bold text-[#344054] text-center">
               Meus dados
@@ -45,14 +46,15 @@ export function MeusDadosModal({
 
           {/*grid de dados */}
           <div className="w-full grid grid-cols-2 gap-y-5 gap-x-4 text-left mb-8">
-
             <div>
               <p className="font-bold text-sm text-[#344054]">CRM</p>
               <p className="text-[#475467] text-sm mt-1">123456/PB</p>
             </div>
 
             <div>
-              <p className="font-bold text-sm text-[#344054]">Frequência de Atendimento</p>
+              <p className="font-bold text-sm text-[#344054]">
+                Frequência de Atendimento
+              </p>
               <p className="text-[#475467] text-sm mt-1">Semanal</p>
             </div>
 
@@ -71,18 +73,16 @@ export function MeusDadosModal({
             <div className="col-span-2">
               <p className="font-bold text-sm text-[#344054]">Endereço</p>
               <p className="text-[#475467] text-sm mt-1 w-[90%] leading-relaxed">
-                Rua Fulano de Tal de Lorem Ipsum da Silva<br></br>12345-000, Esperança - PB
+                Rua Fulano de Tal de Lorem Ipsum da Silva<br></br>12345-000,
+                Esperança - PB
               </p>
             </div>
           </div>
 
-          <Button
-            className="w-full bg-[#F45D6C] hover:bg-[#D44D54] text-white font-medium rounded-full h-12 text-base shadow-none"
-          >
+          <Button className="w-full bg-[#F45D6C] hover:bg-[#D44D54] text-white font-medium rounded-full h-12 text-base shadow-none">
             <LogOut className="mr-2 h-5 w-5 rotate-180" />
             Sair
           </Button>
-
         </div>
       </DialogContent>
     </Dialog>
