155602b8dddc12a4958d53a2ae015e8384960c08
José Thomaz <jose.thoomaz@gmail.com>
2025-11-24 01:39:13 -0300

    Feat:adiciona pequena prévia do arquivo adicionado

--- frontend/atendimento-app/src/components/forms/relatorioForm.tsx
+++ frontend/atendimento-app/src/components/forms/relatorioForm.tsx
@@ -4,12 +4,12 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { DialogFooter } from "@/components/ui/dialog";
 import { Textarea } from "../ui/textarea";
-import { Upload, CirclePlus, Info } from 'lucide-react';
+import { Upload, CirclePlus, Info } from "lucide-react";
 
 export type RelatorioFormData = {
   data: string;
   titulo: string;
-  arquivo?: File;
+  arquivo?: FileList;
   descricao: string;
 };
 
@@ -18,13 +18,16 @@ interface RelatorioFormProps {
 }
 
 export default function RelatorioForm({ onSubmit }: RelatorioFormProps) {
-  const { register, handleSubmit } = useForm<RelatorioFormData>({
+  const { register, handleSubmit, watch } = useForm<RelatorioFormData>({
     defaultValues: {
       data: new Date().toISOString().split("T")[0],
       titulo: "",
       descricao: "",
     },
   });
+
+  const arquivo = watch("arquivo");
+  const previewUrl = arquivo && arquivo[0] ? URL.createObjectURL(arquivo[0]) : null;
 
   return (
     <form
@@ -49,24 +52,42 @@ export default function RelatorioForm({ onSubmit }: RelatorioFormProps) {
         <Label>Inserir arquivo</Label>
 
         <Label
-            htmlFor="arquivo"
-            className="
-            w-full h-[150px] flex flex-col items-center justify-center border-1 border-dashed border-[#B2D7EC] rounded-[30px] cursor-pointer bg-[#F8FAFD]"
-          >
-          <button className="pointer-events-none flex items-center gap-2 px-6 py-2 rounded-full
+          htmlFor="arquivo"
+          className="
+          relative w-full h-[150px] flex flex-col items-center justify-center border-1 border-dashed border-[#B2D7EC] rounded-[30px] cursor-pointer bg-[#F8FAFD] overflow-hidden"
+        >
+          {previewUrl ? (
+            <img
+              src={previewUrl}
+              alt="Prévia"
+              className="w-[200px] h-full object-cover"
+            />
+          ) : (
+            <button className="pointer-events-none flex items-center gap-2 px-6 py-2 rounded-full
               bg-[#0D4F97] text-white font-semibold h-9">
             <Upload className="h-4 w-4 text-white" />
               Enviar arquivo
           </button>
+          )}
           
+          {arquivo && arquivo[0] && (
+            <p className="absolute bottom-0 left-1/2 transform -translate-x-1/2 
+                          bg-white bg-opacity-70 border border-[#B2D7EC]
+                          text-[#344054] text-sm px-3 py-1 rounded-full">
+              {arquivo[0].name}
+            </p>
+          )}
+
         </Label>
         <Input
           id="arquivo"
           type="file"
+          accept="image/*"
           className="hidden"
           {...register("arquivo")}
         />
-      </div>
+ 
+    </div>
 
       <div className="flex items-center gap-3">
         <div className="flex-1 h-[1px] bg-gray-300"></div>
