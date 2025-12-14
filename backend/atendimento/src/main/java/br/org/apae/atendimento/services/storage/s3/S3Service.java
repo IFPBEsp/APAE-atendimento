6bd40ae971f89d6c5e312d70ad57c3f57748c5b9
Filipe Kevyn <FilipeKevyn4@gmail.com>
2025-12-14 16:11:53 -0300

    feat: implementação do sdk s3 para envios de objetos ao bucket

--- backend/atendimento/src/main/java/br/org/apae/atendimento/services/storage/s3/S3Service.java
+++ backend/atendimento/src/main/java/br/org/apae/atendimento/services/storage/s3/S3Service.java
@@ -1,26 +1,33 @@
-package br.org.apae.atendimento.services;
+package br.org.apae.atendimento.services.storage.s3;
 
 import br.org.apae.atendimento.exceptions.MinioStorageException;
-import io.minio.*;
+import br.org.apae.atendimento.services.storage.ObjectStorageService;
+import br.org.apae.atendimento.services.storage.s3.S3PresignedUrlService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.InputStream;
 
 @Service
-public class MinioService {
+@Profile("prod")
+public class S3Service implements ObjectStorageService {
 
-    private final MinioClient client;
+    private final S3Client client;
 
-    private final PresignedUrlService urlService;
+    private final S3PresignedUrlService urlService;
 
     @Value("${bucket.name}")
     private String BUCKET_NAME;
 
     @Autowired
-    public MinioService(MinioClient client, PresignedUrlService urlService) {
+    public S3Service(S3Client client, S3PresignedUrlService urlService) {
         this.client = client;
         this.urlService = urlService;
     }
@@ -44,14 +51,15 @@ public class MinioService {
                 contentType = "application/octet-stream";
             }
 
-            client.putObject(
-                    PutObjectArgs.builder()
-                            .bucket(BUCKET_NAME)
-                            .object(objectName)
-                            .stream(is, file.getSize(), -1)
-                            .contentType(contentType)
-                            .build()
-            );
+            PutObjectRequest request = PutObjectRequest.builder()
+                    .bucket(BUCKET_NAME)
+                    .key(objectName)
+                    .contentType(contentType)
+                    .contentLength(file.getSize())
+                    .build();
+
+            client.putObject(request, RequestBody.fromInputStream(is, file.getSize()));
+
         } catch (Exception e) {
             throw new MinioStorageException("Erro ao enviar arquivo para o bucket", e);
         }
@@ -59,13 +67,18 @@ public class MinioService {
 
     public void deletarArquivo(String objectName){
         try {
-            client.removeObject(RemoveObjectArgs.builder()
+            DeleteObjectRequest deleteRequest = DeleteObjectRequest.builder()
                     .bucket(BUCKET_NAME)
-                    .object(objectName)
-                    .build());
+                    .key(objectName)
+                    .build();
+
+            client.deleteObject(deleteRequest);
+
         } catch (Exception e){
             e.printStackTrace();
             throw new MinioStorageException("Erro ao apagar arquivo", e);
         }
     }
+
+
 }
