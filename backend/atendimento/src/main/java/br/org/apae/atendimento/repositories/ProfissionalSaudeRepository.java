705f0478a09333f1e318459903627225a87f947c
Filipe Kevyn <FilipeKevyn4@gmail.com>
2025-12-15 02:41:08 -0300

    feat: adição de campos bloqueados para atendimento no card de agendamento

--- backend/atendimento/src/main/java/br/org/apae/atendimento/repositories/ProfissionalSaudeRepository.java
+++ backend/atendimento/src/main/java/br/org/apae/atendimento/repositories/ProfissionalSaudeRepository.java
@@ -1,11 +1,14 @@
 package br.org.apae.atendimento.repositories;
 
+import br.org.apae.atendimento.dtos.response.PacienteOptionDTO;
+import br.org.apae.atendimento.entities.Paciente;
 import br.org.apae.atendimento.entities.ProfissionalSaude;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
