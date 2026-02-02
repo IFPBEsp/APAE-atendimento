840556516d7c7b514e06486de4e82133556516fe
Filipe Kevyn <FilipeKevyn4@gmail.com>
2026-02-02 01:42:07 -0300

    chore: adicionando variáveis de ambiente para cada perfil da aplicação

--- backend/atendimento/src/main/java/br/org/apae/atendimento/config/SecurityCorsConfig.java
+++ backend/atendimento/src/main/java/br/org/apae/atendimento/config/SecurityCorsConfig.java
@@ -1,5 +1,6 @@
 package br.org.apae.atendimento.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,13 +12,16 @@ import java.util.List;
 @Configuration
 public class SecurityCorsConfig {
 
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of(
-                "http://localhost:3000"
+                allowedOrigins
         ));
 
         config.setAllowedMethods(List.of(
