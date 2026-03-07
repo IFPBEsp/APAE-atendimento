0f4358ba6fca69b8d3e11e30fad75ee5c01a3789
0xJotave <joaovmb15@gmail.com>
2026-03-07 14:51:24 -0300

    refactor: change rules of commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   extends: ["@commitlint/config-conventional"],
 
   rules: {
@@ -24,30 +24,9 @@ export default {
         "remove",
       ],
     ],
-
-    /*
-    ESCOPO OBRIGATÓRIO
-    */
-    "scope-empty": [2, "never"],
-
     /*
     DESCRIÇÃO OBRIGATÓRIA
     */
     "subject-empty": [2, "never"],
-
-    /*
-    DESCRIÇÃO MÍNIMA
-    */
-    "subject-min-length": [2, "always", 10],
-
-    /*
-    LOWERCASE
-    */
-    "subject-case": [2, "always", ["lower-case"]],
-
-    /*
-    BLOQUEAR PONTO FINAL
-    */
-    "subject-full-stop": [2, "never", "."],
   },
 };
