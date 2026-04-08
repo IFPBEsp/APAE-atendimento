113c722f2edae9a0f460dfae894f52523f1bb21c
0xJotave <joaovmb15@gmail.com>
2026-04-08 01:38:57 -0300

    fix: merge bugs

--- backend/atendimento/src/main/java/br/org/apae/atendimento/exceptions/handler/GlobalExceptionHandler.java
+++ backend/atendimento/src/main/java/br/org/apae/atendimento/exceptions/handler/GlobalExceptionHandler.java
@@ -79,6 +79,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NoResourceFoundException.class)
     public ResponseEntity<ErrorResponse> handleNoResourceFound(NoResourceFoundException ex) {
         return buildResponse(HttpStatus.NOT_FOUND, "Recurso não encontrado.");
+    }
     @ExceptionHandler(org.springframework.web.server.ResponseStatusException.class)
     public ResponseEntity<ErrorResponse> handleResponseStatusException(org.springframework.web.server.ResponseStatusException ex) {
         HttpStatus status = HttpStatus.valueOf(ex.getStatusCode().value());
