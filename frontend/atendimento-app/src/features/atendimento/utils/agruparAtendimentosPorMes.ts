345f570aabbf7a6e611b881c01b9bfaebb78d44a
Ruan Gouveia <ruangouveia78@gmail.com>
2026-09-10 18:04:05 -0300

    fix[frontend]: ordenar lista decrescentemente antes do agrupamento por mes

--- frontend/atendimento-app/src/features/atendimento/utils/agruparAtendimentosPorMes.ts
+++ frontend/atendimento-app/src/features/atendimento/utils/agruparAtendimentosPorMes.ts
@@ -3,7 +3,16 @@ import { Atendimento } from "../types";
 export default function agruparPorMes(lista: Atendimento[]) {
   const meses: Record<string, Atendimento[]> = {};
 
-  lista.forEach((item) => {
+  const listaOrdenada = [...lista].sort((a, b) => {
+    const [diaA, mesA, anoA] = a.data.split("/");
+    const dataA = new Date(Number(anoA), Number(mesA) - 1, Number(diaA));
+    const [diaB, mesB, anoB] = b.data.split("/");
+    const dataB = new Date(Number(anoB), Number(mesB) - 1, Number(diaB));
+    
+    return dataB.getTime() - dataA.getTime();
+  });
+
+  listaOrdenada.forEach((item) => {
     const [, mes, ano] = item.data.split("/");
 
     const nomeMes = new Date(Number(ano), Number(mes) - 1).toLocaleString(
